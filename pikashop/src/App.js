1fe00f60632bb576553fef4328f9fae1602dd324
Muhammad M. Mostey <muhammadmostey@gmail.com>
2023-11-14 21:24:14 +0300

    creates the react with redux, and adds files structure

---
+++ pikashop/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Layout from './components/Layout';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+
+
+
+
+function App() {
+  return (
+    // <Routes>
+    //   <Route path='/' element={<div>Hello, Muhammad</div>} />
+    //   <Route path='/about' element={<div>Contact</div>} />
+    // </Routes>
+  );
+}
+
+export default App;
