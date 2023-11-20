6d72e47e828e167c50b0ab30e1fd0b6ce9b17b74
Muhammad Mostey <muhammadmostey@gmail.com>
2023-11-20 23:03:25 +0300

    adds header section to header component

--- pikashop/src/components/Header.js
+++ pikashop/src/components/Header.js
@@ -1,7 +1,42 @@
-function Header(){
-    return(<h1>Header</h1>);
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+import { BiSearchAlt } from "react-icons/bi";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <div className="content pt-4 main-header gap-2 items-center">
+
+
+          <div className="basis-1/5 justify-start items-center block">
+            <span className="text-base md:text-lg lg:text-xl xl:text-2xl inline-block">PikaExpress</span>
+          </div>
+
+
+          <div className="flex search-div basis-1/2 ">
+            
+            <input
+              type="text"
+              className="search-text m-2 pl-2 "
+              placeholder="Search for your product now 👋🏻"
+              aria-label="Search for your product now"
+              autoComplete="off"
+              maxlength="60"
+            />
+
+            <span className="mr-2 self-center">
+              <BiSearchAlt className="w-12 h-8 bg-black rounded-2xl p-1" />{" "}
+            </span>
+          </div>
+
+
+          <div className="flex basis-1/3 justify-end">Hi</div>
+        </div>
+      </header>
+    </>
+  );
 }
-
-
 
 export default Header;
